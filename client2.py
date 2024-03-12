eff1f248249b000d1bda70171dffb04f86fdedef
indra-code <144906869+indra-code@users.noreply.github.com>
2024-03-12 20:06:55 +0530

    Update client2.py

--- client2.py
+++ client2.py
@@ -1,15 +1,22 @@
 import os
 import socket
+import ssl
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost", 9999))
+ipaddr = "localhost" #use your ip address
+client.connect((ipaddr, 9999))
+certificate_data = client.recv(4096)
+cert_file_path = "server.crt"
+with open(cert_file_path, "wb") as cert_file:
+    cert_file.write(certificate_data)
+client_ssl = ssl.wrap_socket(client,ca_certs="./server.crt")
 
-f_name = "received_test.py"  # Define the file name
+f_name = "received_test.c"  # Define the file name
 # file_size = os.path.getsize("test.c")  # Get the file size
 file_name = f_name +"<END_FILENAME>"
 # # Send file name
-client.send((file_name).encode())
-t = client.recv(1024)
+client_ssl.send((file_name).encode())
+t = client_ssl.recv(1024)
 print(t.decode())
 # client.settimeout(20)
 
@@ -17,18 +24,19 @@ print(t.decode())
 #client.send(str(file_size).encode())
 
 # Send file content
-with open("test.py", "rb") as file:
+with open("test.c", "rb") as file:
     while True:
         data = file.read(1024)
         if not data:
             break
-        client.sendall(data)
+        client_ssl.sendall(data)
 
 # Send end marker
-client.send(b"<END>")
-r = client.recv(1024)
+client_ssl.send(b"<END>")
+r = client_ssl.recv(1024)
 print(r.decode())
-output = client.recv(4096).decode()
+output = client_ssl.recv(4096).decode()
 print("Output:")
 print(output)
+client_ssl.close()
 
